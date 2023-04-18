f71be6eda674a2e6bc232e083868e7407c7e2558
Hawa701 <108014859+Hawa701@users.noreply.github.com>
2023-04-18 20:55:45 +0300

    Start Page is done
    * Start page is now fully functional and responsive.
    * File structure is also changed
    * Added a "Start Page" HTML, CSS, and JS

---
+++ Website/Script/Start Page.js
@@ -0,0 +1,6 @@
+//When start button is clicked
+var startBtn = document.getElementById("startBtn");
+
+startBtn.addEventListener("click", function () {
+  window.location.assign("/Website/HTML/Main Page.html");
+});
